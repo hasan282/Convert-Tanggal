5e6f086dacd6a8c936db3c97bc2290beba202209
Hasan Abdullah <47323055+hasan282@users.noreply.github.com>
2019-06-18 11:26:06 +0700

    Add Some Case
    Examples :
    28-02-2019 to 28 Februari 2019

--- ConvrTanggal JAVA/ConvertTanggal/src/converttanggal/ConvertTanggal.java
+++ ConvrTanggal JAVA/ConvertTanggal/src/converttanggal/ConvertTanggal.java
@@ -5,9 +5,9 @@ public class ConvertTanggal {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        String MyDate = "2015-08-17";
+        String MyDate = "14 Juni 2018";
         
-        System.out.println(FnTanggal.Convert(20, MyDate));
+        System.out.println(FnTanggal.Convert(42, MyDate));
     }
     
 }
--- ConvrTanggal JAVA/ConvertTanggal/src/converttanggal/FnTanggal.java
+++ ConvrTanggal JAVA/ConvertTanggal/src/converttanggal/FnTanggal.java
@@ -3,20 +3,84 @@ package converttanggal;
 public class FnTanggal {
     public static String Convert(int Tipe, String Target) {
         String ListBulan = "Januari@@Februari@@Maret@@April@@Mei@@Juni@@Juli@@Agustus@@September@@Oktober@@November@@Desember";
-        String[] Bulan = ListBulan.split("@@");
-        String Result = null;
-        
-        if(Tipe==0) {
-            Result = null;
-        }
-        else if(Tipe==10) {
-            String[] Tanggal = Target.split("-");
-            Result = Tanggal[2] + "-" + Tanggal[1] + "-" + Tanggal[0];
-        }
-        else if(Tipe==20) {
-            String[] Tanggal = Target.split("-");
-            int Month = Integer.parseInt(Tanggal[1]);
-            Result = Tanggal[2] + " " + Bulan[Month-1] + " " + Tanggal[0];
+        String BulanEng = "January@@February@@March@@April@@May@@June@@July@@August@@September@@October@@November@@December";
+        String NmbMonth = "01@@02@@03@@04@@05@@06@@07@@08@@09@@10@@11@@12";
+        String[] NoBulan = NmbMonth.split("@@");
+        String[] Bulan;
+        String Result;
+        switch (Tipe) {
+            case 0:
+                Result = null;break;
+            case 10:
+                // Convert : 28-02-2019 --> 2019-02-28
+                // Convert : 2019-02-28 --> 28-02-2019
+                {String[] Tanggal = Target.split("-");Result = Tanggal[2] + "-" + Tanggal[1] + "-" + Tanggal[0];break;}
+            case 11:
+                // Convert : 28-02-2019 --> 02-28-2019
+                // Convert : 02-28-2019 --> 28-02-2019
+                {String[] Tanggal = Target.split("-");Result = Tanggal[1] + "-" + Tanggal[0] + "-" + Tanggal[2];break;}
+            case 12:
+                // Convert : 28/02/2019 --> 2019/02/28
+                // Convert : 2019/02/28 --> 28/02/2019
+                {String[] Tanggal = Target.split("/");Result = Tanggal[2] + "/" + Tanggal[1] + "/" + Tanggal[0];break;}
+            case 13:
+                // Convert : 28/02/2019 --> 02/28/2019
+                // Convert : 02/28/2019 --> 28/02/2019
+                {String[] Tanggal = Target.split("/");Result = Tanggal[1] + "/" + Tanggal[0] + "/" + Tanggal[2];break;}
+            case 20:
+                // Convert : 28/02/2019 --> 28-02-2019
+                {String[] Tanggal = Target.split("/");Result = Tanggal[0] + "-" + Tanggal[1] + "-" + Tanggal[2];break;}
+            case 21:
+                // Convert : 28-02-2019 --> 28/02/2019
+                {String[] Tanggal = Target.split("-");Result = Tanggal[0] + "/" + Tanggal[1] + "/" + Tanggal[2];break;}
+            case 22:
+                // Convert : 28.02.2019 --> 28-02-2019
+                {String[] Tanggal = Target.split(".");Result = Tanggal[0] + "-" + Tanggal[1] + "-" + Tanggal[2];break;}
+            case 23:
+                // Convert : 28-02-2019 --> 28.02.2019
+                {String[] Tanggal = Target.split("-");Result = Tanggal[0] + "." + Tanggal[1] + "." + Tanggal[2];break;}
+            case 30:
+                // Convert : 2019-02-28 --> 28 Februari 2019
+                {Bulan = ListBulan.split("@@");String[] Tanggal = Target.split("-");int Month = Integer.parseInt(Tanggal[1]);
+                Result = Tanggal[2] + " " + Bulan[Month-1] + " " + Tanggal[0];break;}
+            case 31:
+                // Convert : 28-02-2019 --> 28 Februari 2019
+                {Bulan = ListBulan.split("@@");String[] Tanggal = Target.split("-");int Month = Integer.parseInt(Tanggal[1]);
+                Result = Tanggal[0] + " " + Bulan[Month-1] + " " + Tanggal[2];break;}
+            case 32:
+                // Convert : 02-28-2019 --> 28 Februari 2019
+                {Bulan = ListBulan.split("@@");String[] Tanggal = Target.split("-");int Month = Integer.parseInt(Tanggal[0]);
+                Result = Tanggal[1] + " " + Bulan[Month-1] + " " + Tanggal[2];break;}
+            case 33:
+                // Convert : 2019-02-28 --> 28 February 2019 (English)
+                {Bulan = BulanEng.split("@@");String[] Tanggal = Target.split("-");int Month = Integer.parseInt(Tanggal[1]);
+                Result = Tanggal[2] + " " + Bulan[Month-1] + " " + Tanggal[0];break;}
+            case 34:
+                // Convert : 2019-02-28 --> 2019 February 28 (English)
+                {Bulan = BulanEng.split("@@");String[] Tanggal = Target.split("-");int Month = Integer.parseInt(Tanggal[1]);
+                Result = Tanggal[0] + " " + Bulan[Month-1] + " " + Tanggal[2];break;}
+            case 35:
+                // Convert : 2019-02-28 --> February 28, 2019 (English)
+                {Bulan = BulanEng.split("@@");String[] Tanggal = Target.split("-");int Month = Integer.parseInt(Tanggal[1]);
+                Result = Bulan[Month-1] + " " + Tanggal[2] + ", " + Tanggal[0];break;}
+            case 40:
+                // Convert : 28 Februari 2019 --> 28-02-2019
+                {Bulan = ListBulan.split("@@");String[] Tanggal = Target.split("\\s+");String Month = null;
+                for(int b = 0; b <= Bulan.length; b++){if(Tanggal[1].equals(Bulan[b])){Month = NoBulan[b];break;}}
+                Result = Tanggal[0] + "-" + Month + "-" + Tanggal[2];break;}
+            case 41:
+                // Convert : 28 Februari 2019 --> 2019-02-28
+                {Bulan = ListBulan.split("@@");String[] Tanggal = Target.split("\\s+");String Month = null;
+                for(int b = 0; b <= Bulan.length; b++){if(Tanggal[1].equals(Bulan[b])){Month = NoBulan[b];break;}}
+                Result = Tanggal[2] + "-" + Month + "-" + Tanggal[0];break;}
+            case 42:
+                // Convert : 28 Februari 2019 --> 28-02-2019
+                {Bulan = ListBulan.split("@@");String[] Tanggal = Target.split("\\s+");String Month = null;
+                for(int b = 0; b <= Bulan.length; b++){if(Tanggal[1].equals(Bulan[b])){Month = NoBulan[b];break;}}
+                Result = Month + "-" + Tanggal[0] + "-" + Tanggal[2];break;}
+            default:
+                Result = null;
+                break;
         }
         return Result;
     }
